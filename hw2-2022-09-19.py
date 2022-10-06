66feca9b1c5cfb124d50012f798e0a1282033d73
nikolaevkb <nikolaev.kiril@inbox.ru>
2022-10-06 12:33:44 +0300

    Create hw2-2022-09-19.py

---
+++ hw2-2022-09-19.py
@@ -0,0 +1,14 @@
+letters = 'Who keeps company with the wolf, will learn to howl.'
+template = 'w'
+exclude = 'l'
+
+sym = 0
+for letters in 'Who keeps company with the wolf, will learn to howl.':
+    if letters == template:
+        sym +=1
+print(sym)
+
+
+str = 'Who keeps company with the wolf, will learn to howl.'
+res_str = str.replace(exclude, '')
+print(res_str)
