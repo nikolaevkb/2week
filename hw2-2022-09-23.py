b0b63523d3f9ff6dabdff252a6c16203da576d1f
nikolaevkb <nikolaev.kiril@inbox.ru>
2022-10-09 23:32:03 +0300

    Update hw2-2022-09-23.py

--- hw2-2022-09-23.py
+++ hw2-2022-09-23.py
@@ -1,1 +1,16 @@
-array
+import shutil
+import os.path
+
+count_lines = {}
+for line in open(r'q4_urls.txt', encoding="utf8"):
+    clean_line = line.rstrip()
+    if clean_line not in count_lines:
+        count_lines[clean_line] = 0
+    count_lines[clean_line] += 1
+
+out = open(r'new.txt', 'w')
+for line in open(r'q4_urls.txt'):
+    count = count_lines[line.rstrip()]
+    out.write(f"{line} | {count}")
+
+out.close()
