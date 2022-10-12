fa5d5c9d3d5a8d662c5bae6c7987b8e4789b1839
nikolaevkb <nikolaev.kiril@inbox.ru>
2022-10-13 00:42:25 +0300

    Update hw2-2022-09-23.py

--- hw2-2022-09-23.py
+++ hw2-2022-09-23.py
@@ -1,16 +1,28 @@
-import shutil
-import os.path
+# count_lines = {}
+# for line in open(r'q41.txt'):
+#     clean_line = line.rstrip()
+#     if clean_line not in count_lines:
+#         count_lines[clean_line] = 0
+#     count_lines[clean_line] += 1
+#
+# out = open(r'new.txt', 'w')
+# for line in open(r'q41.txt'):
+#     count = count_lines[line.rstrip()]
+# for key, item in sorted(count_lines.items(), key=lambda x: x[1], reverse=True):
+#     out.write(f"{key} | {item}")
+#
+# out.close()
 
 count_lines = {}
-for line in open(r'q4_urls.txt', encoding="utf8"):
+for line in open(r'q41.txt'):
     clean_line = line.rstrip()
     if clean_line not in count_lines:
         count_lines[clean_line] = 0
     count_lines[clean_line] += 1
 
-out = open(r'new.txt', 'w')
-for line in open(r'q4_urls.txt'):
+out = open(r'new.txt', "w")
+for line in open(r'q41.txt'):
     count = count_lines[line.rstrip()]
-    out.write(f"{line} | {count}")
+    out.write(f"{line} ! {count}")
 
 out.close()
